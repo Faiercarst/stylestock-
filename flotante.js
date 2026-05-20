5135462adc6ea0d5e8f4a8f97348251e465796b4
Faiercarst <1012918580@teinco.edu.co>
2026-05-20 14:58:40 -0500

    mejoras en publisidad

--- flotante.js
+++ flotante.js
@@ -7,9 +7,13 @@ contenedor.innerHTML = `
       <a href="https://www.facebook.com/profile.php?id=61589835586603" target="_blank" class="menu-item facebook">
    <img src="Facebook_Logo_(2019).png" width="20"> Facebook
 </a>
-      <a href="https://www.instagram.com/stylestock/"  target="_blank" class="menu-item instagram">
-         <img src="Instagram_icon.png" width="20"> Instagram
-      </a>
+      <a href="https://www.instagram.com/stylestock436/" target="_blank" class="menu-item instagram">
+   <img src="Instagram_icon.png" width="20"> Instagram
+</a>
+<a href="https://mail.google.com/mail/u/0/?view=cm&to=stylestock436@gmail.com" target="_blank" class="menu-item correo">
+   <img src="Gmail_icon_(2020).svg.png" width="20"> Correo
+</a>
+
     </div>
   </div>
     
